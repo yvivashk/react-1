81503e47a354933652cbca2acd933583cfdba46b
yvivashk <93151244+yvivashk@users.noreply.github.com>
2022-05-30 22:55:35 +0300

    В файле App.js создал список, а в файле App.css прописал стили к этому списку

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className="list">
+        <li><a className="link" href="#">css</a></li>
+        <li><a className="link" href="#">html</a></li>
+        <li><a className="link" href="#">js</a></li>
+        <li><a className="link" href="#">react</a></li>
+      </ul>
     </div>
   );
 }
