d70c685d668c028ee7dfa552f6665bce64a0f72f
Yura Ivashkov <yvivashk@mail.ru>
2022-06-07 16:14:12 +0300

    В файле App.js - создал четыре компоненты App(), Header(), Main(), Footer(), в файле App.css - прописал стили к компонентам

--- src/App.js
+++ src/App.js
@@ -1,17 +1,75 @@
-import React, { Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <ul className="list">
-        <li><a className="link" href="#">css</a></li>
-        <li><a className="link" href="#">html</a></li>
-        <li><a className="link" href="#">js</a></li>
-        <li><a className="link" href="#">react</a></li>
-      </ul>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
-}
+};
+
+// Компонента Хедер
+const Header = () => {
+  return (
+    <ul class="list header">
+      <li>
+        <a class="link" href="#">
+          html
+        </a>
+      </li>
+      <li>
+        <a class="link" href="#">
+          css
+        </a>
+      </li>
+      <li>
+        <a class="link" href="#">
+          js
+        </a>
+      </li>
+      <li>
+        <a class="link" href="#">
+          React
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+// Компонента Майн
+const Main = () => {
+  return <section class="main">Основное содержимое</section>;
+};
+
+// Компонента футер
+const Footer = () => {
+  return (
+    <ul class="list footer">
+      <li>
+        <a class="link" href="">
+          О нас
+        </a>
+      </li>
+      <li>
+        <a class="link" href="">
+          Контакты
+        </a>
+      </li>
+      <li>
+        <a class="link" href="">
+          Напишите нам
+        </a>
+      </li>
+      <li>
+        <a class="link" href="">
+          Лого
+        </a>
+      </li>
+    </ul>
+  );
+};
 
 export default App;
