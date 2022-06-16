f211061234dc8b6838ea05900917414326183c70
Yura Ivashkov <yvivashk@mail.ru>
2022-06-16 17:09:39 +0300

    В файле App.js - создал новую структуру html, в файле App.css - прописал стили, добавил новое изображение

--- src/App.js
+++ src/App.js
@@ -3,10 +3,45 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+    <div className="content-wrapper">
+      <header className="header">
+        <img
+          src="cloud-computing.png"
+          width="100"
+          height="100"
+          alt="логотип"
+        ></img>
+      </header>
+      <section className="sidebar-menu">
+        <ul className="list">
+          <li>
+            <a className="link" href="#">
+              Profile
+            </a>
+          </li>
+          <li>
+            <a className="link" href="#">
+              Messages
+            </a>
+          </li>
+          <li>
+            <a className="link" href="#">
+              News
+            </a>
+          </li>
+          <li>
+            <a className="link" href="#">
+              Music
+            </a>
+          </li>
+          <li>
+            <a className="link" href="#">
+              Settings
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section className="main"></section>
     </div>
   );
 };
